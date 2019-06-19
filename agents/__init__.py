a1ec050fb3897d9d04bdeec472da880b1201a894
Taylor Childers <jchilders@alcfwl058.alcf.anl.gov>
2019-06-19 09:28:10 -0500

    adding imports to init

---
+++ agents/__init__.py
@@ -0,0 +1,6 @@
+
+from .simple_agent import SimpleAgent
+from .random_agent import RandomAgent
+from .leaf_agent import LeafAgent
+from .forward_agent import ForwardAgent
+from .backward_agent import BackwardAgent
